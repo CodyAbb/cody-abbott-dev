78305204ee027544921d13a8597283e16348ae99
Cody Abbott <cody.abbott16@gmail.com>
2020-06-12 15:40:04 +0100

    moved dummy data into content container

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,6 +1,13 @@
 import React from "react"
 import Navbar from "../components/navbar"
+import ContentContainer from "../components/contentcontainer"
 
 export default function Posts() {
-  return <Navbar>This will include posts based on coding</Navbar>
+  return (
+    <Navbar>
+      <ContentContainer>
+        This will include posts based on coding
+      </ContentContainer>
+    </Navbar>
+  )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Navbar from "../components/navbar"
+import ContentContainer from "../components/contentcontainer"
 
 export default function Projects() {
   return (
     <Navbar>
-      <main>Here are the projects I completed at CodeClan</main>
+      <ContentContainer>
+        <main>Here are the projects I completed at CodeClan</main>
+      </ContentContainer>
     </Navbar>
   )
 }
