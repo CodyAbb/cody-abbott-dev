92916fa2bab5e9b41821770342457c2f455e1091
Cody Abbott <cody.abbott16@gmail.com>
2020-06-12 13:39:01 +0100

    base navbar styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import style from "./navbar.module.css"
 
 export default function Navbar({ children }) {
   return (
     <>
-      <nav>
-        <Link to="/">Cody Abbott</Link>
-        <Link to="/posts/">Posts/Tutorials</Link>
-        <Link to="/projects/">Projects</Link>
+      <nav className={style.verticalnav}>
+        <Link to="/" className={style.navlink}>
+          <h2>Cody Abbott</h2>
+        </Link>
+        <Link to="/posts/" className={style.navlink}>
+          <h2>Posts/Tutorials</h2>
+        </Link>
+        <Link to="/projects/" className={style.navlink}>
+          <h2>Projects</h2>
+        </Link>
       </nav>
       {children}
     </>
