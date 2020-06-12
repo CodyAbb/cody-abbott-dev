bafaf2032d8c7fa0b3c1eb49c36c5f685a374c78
Cody Abbott <cody.abbott16@gmail.com>
2020-06-12 20:55:24 +0100

    contains new snapshot from jest and comments

--- src/components/__tests__/navbar.js
+++ src/components/__tests__/navbar.js
@@ -1,11 +1,26 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import Navbar from "../navbar"
+import { PureNavbar as Navbar } from "../navbar"
 describe("Navbar", () => {
   it("renders correctly", () => {
-    const tree = renderer
-      .create(<Navbar siteTitle="Default Starter" />)
-      .toJSON()
+    //Created using the query from Header.js
+    const data = {
+      file: {
+        childImageSharp: {
+          fixed: {
+            base64:
+              "data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAUABQDASIAAhEBAxEB/8QAGQABAAMBAQAAAAAAAAAAAAAAAAECBAMF/8QAFwEAAwEAAAAAAAAAAAAAAAAAAQIDAP/aAAwDAQACEAMQAAABzavPhX2KsJ7gewnP/8QAGhAAAgMBAQAAAAAAAAAAAAAAAQIAAxEhM//aAAgBAQABBQKz04ENxSOZpjYxo7K9LhjP/8QAFhEBAQEAAAAAAAAAAAAAAAAAAAEQ/9oACAEDAQE/Aciv/8QAFhEBAQEAAAAAAAAAAAAAAAAAEAER/9oACAECAQE/AcKf/8QAHBABAAICAwEAAAAAAAAAAAAAAQARAiEQMUFR/9oACAEBAAY/ApkYen2UVwVctNxIiup3P//EAB4QAAMAAgEFAAAAAAAAAAAAAAABESExQVFhcYHh/9oACAEBAAE/IX9IidKrkN7R3o12aKc5EppRnAmVr4NHLRUl8nwf/9oADAMBAAIAAwAAABBL+D7/xAAYEQACAwAAAAAAAAAAAAAAAAAAMQEQEf/aAAgBAwEBPxDXSjSf/8QAFxEBAQEBAAAAAAAAAAAAAAAAABFBAf/aAAgBAgEBPxDC8bV//8QAHhABAAICAwADAAAAAAAAAAAAARExACFBUXFhgcH/2gAIAQEAAT8Q1JCQSVp3vGYgkR0M7w2mAi4448xNmHcV3f3kpCJq76MipBBwb/c3JIViD5Y+kXQKxj2ICnfmf//Z",
+            width: 125,
+            height: 125,
+            src:
+              "/static/3ede9b91227891490f9ae0fdf9ba25db/bac10/facebook_profile_pic.jpg",
+            srcSet:
+              "/static/3ede9b91227891490f9ae0fdf9ba25db/bac10/facebook_profile_pic.jpg 1x,\n/static/3ede9b91227891490f9ae0fdf9ba25db/1b59b/facebook_profile_pic.jpg 1.5x,\n/static/3ede9b91227891490f9ae0fdf9ba25db/034c8/facebook_profile_pic.jpg 2x",
+          },
+        },
+      },
+    }
+    const tree = renderer.create(<Navbar data={data} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,36 @@ import { useStaticQuery, graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import style from "./navbar.module.css"
 
-const Navbar = ({ children }) => {
+// This purenavbar is a wraper that does not contain the graphql query
+// this is included so that Jest can be given dummy graphql query response
+// and continue with the other tests
+export const PureNavbar = ({ data, children }) => (
+  <>
+    <nav className={style.verticalnav}>
+      <Link to="/" className={style.avatarcard}>
+        <Img
+          className={style.avatar}
+          fixed={data.file.childImageSharp.fixed}
+          alt=""
+        />
+        <p className={style.avatartext}>
+          Cody Abbott
+          <br />
+          Software Developer
+        </p>
+      </Link>
+      <Link to="/posts/" className={style.navlink}>
+        <p>Posts/Tutorials</p>
+      </Link>
+      <Link to="/projects/" className={style.navlink}>
+        <p>Projects</p>
+      </Link>
+    </nav>
+    {children}
+  </>
+)
+
+const Navbar = ({ props, children }) => {
   const data = useStaticQuery(
     graphql`
       query {
@@ -18,31 +47,7 @@ const Navbar = ({ children }) => {
     `
   )
 
-  return (
-    <>
-      <nav className={style.verticalnav}>
-        <Link to="/" className={style.avatarcard}>
-          <Img
-            className={style.avatar}
-            fixed={data.file.childImageSharp.fixed}
-            alt=""
-          />
-          <p className={style.avatartext}>
-            Cody Abbott
-            <br />
-            Software Developer
-          </p>
-        </Link>
-        <Link to="/posts/" className={style.navlink}>
-          <p>Posts/Tutorials</p>
-        </Link>
-        <Link to="/projects/" className={style.navlink}>
-          <p>Projects</p>
-        </Link>
-      </nav>
-      {children}
-    </>
-  )
+  return <PureNavbar {...props} children={children} data={data}></PureNavbar>
 }
 
 export default Navbar
