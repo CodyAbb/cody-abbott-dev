4c8c94620dd336866171c00435e30e55a5cb4fd7
Cody Abbott <cody.abbott16@gmail.com>
2020-06-15 16:37:48 +0100

    added back link before audit

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import ContentContainer from "../components/contentcontainer"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
@@ -10,6 +10,9 @@ export default function BlogPost({ data }) {
       <ContentContainer>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div>
+          <Link to="/projects/">&#8592; Back</Link>
+        </div>
       </ContentContainer>
     </Navbar>
   )
