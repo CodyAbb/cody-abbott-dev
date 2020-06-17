159db031757ac1064724849dc85286aab9e7e8e6
Cody Abbott <cody.abbott16@gmail.com>
2020-06-17 15:52:45 +0100

    more adjustments to cards, pre image

--- src/components/articleCard.js
+++ src/components/articleCard.js
@@ -4,18 +4,16 @@ import style from "../pages/projects.module.css"
 
 export default function ArticleCard({ node }) {
   return (
-    <div className={style.card}>
-      <Link to={node.fields.slug}>
-        {/* <img
+    <Link to={node.fields.slug} className={style.card}>
+      {/* <img
         src={node.frontmatter.featuredImage}
         alt="screnshot for spending tracker app"
       /> */}
-        <h3>Image Place Holder</h3>
-        <div className={style.cardtextcontainer}>
-          <h3>{node.frontmatter.title}</h3>
-          <p>{node.frontmatter.lang}</p>
-        </div>
-      </Link>
-    </div>
+      <h3>Image Place Holder</h3>
+      <div className={style.cardtextcontainer}>
+        <h3>{node.frontmatter.title}</h3>
+        <p>{node.frontmatter.lang}</p>
+      </div>
+    </Link>
   )
 }
