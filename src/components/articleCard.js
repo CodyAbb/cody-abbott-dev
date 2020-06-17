7d363642338fdb7f08bc72451d92766ae9ba43b0
Cody Abbott <cody.abbott16@gmail.com>
2020-06-17 14:22:18 +0100

    basic card css styling

--- src/components/articleCard.js
+++ src/components/articleCard.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import style from "../pages/projects.module.css"
 
 export default function ArticleCard({ node }) {
   return (
-    <Link to={node.fields.slug}>
-      {/* <img
+    <div className={style.card}>
+      <Link to={node.fields.slug}>
+        {/* <img
         src={node.frontmatter.featuredImage}
         alt="screnshot for spending tracker app"
       /> */}
-      <h3>
-        {node.frontmatter.title} - {node.frontmatter.date}
-      </h3>
-      <p>{node.frontmatter.lang}</p>
-    </Link>
+        <h3>Image Place Holder</h3>
+        <div className={style.cardtextcontainer}>
+          <h3>{node.frontmatter.title}</h3>
+          <p>{node.frontmatter.lang}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,13 +3,14 @@ import Navbar from "../components/navbar"
 import ContentContainer from "../components/contentcontainer"
 import { graphql } from "gatsby"
 import ArticleCard from "../components/articleCard"
+import style from "./projects.module.css"
 
 export default function Projects({ data }) {
   return (
     <Navbar>
       <ContentContainer>
-        <main>
-          <h2>Here are the projects I completed at CodeClan</h2>
+        <main className={style.container}>
+          <h2 className={style.title}>Projects, CodeClan and beyond...</h2>
           {data.allMarkdownRemark.edges.map(({ node }) => (
             <ArticleCard key={node.id} node={node} />
           ))}
