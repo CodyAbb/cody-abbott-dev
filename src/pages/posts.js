0139a10525b2f91aac259ee255040dae27286164
Cody Abbott <cody.abbott16@gmail.com>
2020-06-18 14:44:05 +0100

    added some basic posts spacing

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,11 +1,18 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import ContentContainer from "../components/contentcontainer"
+import style from "./posts.module.css"
 
 export default function Posts() {
   return (
     <Navbar>
-      <ContentContainer>Content incoming...</ContentContainer>
+      <ContentContainer>
+        <div className={style.container}>
+          <heading>
+            <h2 className={style.title}>Content Incoming...</h2>
+          </heading>
+        </div>
+      </ContentContainer>
     </Navbar>
   )
 }
